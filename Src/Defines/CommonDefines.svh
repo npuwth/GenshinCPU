cc183fce3191942e7bec546de78d823f49e0a94d
Juan <jshmjjx@126.com>
2021-03-20 20:20:47 +0800

    大家可以看一下 写一下意见 之后我应该会写interface接口

---
+++ Src/Defines/CommonDefines.svh
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright(C) Team . Open source License: MIT.
+// ALL RIGHT RESERVED
+// File name   : CPU_Defines.svh
+// Author      : Juan Jiang
+// Date        : 2021-03-20
+// Version     : 0.1
+// Description :
+// none
+//    
+// Parameter   :没有
+//    ...
+//    ...
+// IO Port     :没有
+//    ...
+//    ...
+// Modification History:
+//   Date   |   Author   |   Version   |   Change Description
+//==============================================================================
+// 19-06-02 |    Zion    |     0.1     | Original Version
+// ...
+////////////////////////////////////////////////////////////////////////////////
+`ifndef CommonDefines_SVH
+`define CommonDefines_SVH
+
+
+
+`endif CommonDefines_SVH
