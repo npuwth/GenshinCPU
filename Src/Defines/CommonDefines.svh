70c53a67d5f637245d09f5e52fc13236dd6573c6
三氧化二砷 <7480262+ttjs_yyt123@user.noreply.gitee.com>
2021-03-29 22:30:40 +0800

    传了几个宏定义上来，感觉大家都能用得到，所以先传上来了。剩下的就等我们都写的差不读之后再一起传上来，现在先各人写各人的

--- Src/Defines/CommonDefines.svh
+++ Src/Defines/CommonDefines.svh
@@ -24,7 +24,8 @@
 `define CommonDefines_SVH
 
 // 这两个定义，感觉大家都能用得到，所以先传上来了。剩下的就等我们都写的差不读之后再一起传上来，现在先各人写各人的
-`define WriteEnable 1'b1          // 打开写使能信号
-`define WriteDisable 1'b1         // 关闭写使能信号
-
+`define WriteEnable         1'b1     // 打开写使能信号
+`define WriteDisable        1'b1     // 关闭写使能信号
+`define RstEnable           1'b0     // 打开复位信号(低有效)
+`define RstDisable          1'b1     // 关闭复位信号
 `endif 
