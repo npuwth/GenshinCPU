af4de460bc1fcd04ea18cf2a917d602aec0a1a27
jjx <jshmjjx@126.com>
2021-07-12 12:20:49 +0800

    Utility already

--- Src/NewCache/Cache.sv
+++ Src/NewCache/Cache.sv
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-29 23:11:11
- * @LastEditTime: 2021-07-11 20:27:53
+ * @LastEditTime: 2021-07-12 11:29:37
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Src\ICache.sv
@@ -36,20 +36,79 @@ module Cache #(
     
     
 );
-localparam int unsigned INDEX_WIDTH  = $clog2(SET_NUM) ;
-localparam int unsigned OFFSET_WIDTH = $clog2(LINE_WORD_NUM*4);
-localparam int unsigned TAG_WIDTH    = 32-INDEX_WIDTH-OFFSET_WIDTH ;
+localparam int unsigned BYTES_PER_WORD = 4;
+localparam int unsigned INDEX_WIDTH    = $clog2(SET_NUM) ;
+localparam int unsigned OFFSET_WIDTH   = $clog2(LINE_WORD_NUM*BYTES_PER_WORD);//
+localparam int unsigned TAG_WIDTH      = 32-INDEX_WIDTH-OFFSET_WIDTH ;
 
 
 
 typedef struct packed {
     logic valid;
     logic dirty;
-    logic [`TAGBITNUM-1:0] tag;  
-} tag_t; //每一路 一个tag_t变量
+    logic [TAG_WIDTH-1:0] tag;  
+} tagv_t; //每一路 一个tag_t变量
 
-typedef logic [LINE_WORD_NUM-1:0][DATA_WIDTH-1:0] lint_t;//每一路一个cache_line
+typedef logic [TAG_WIDTH-1:0]                     tag_t;
 typedef logic [INDEX_WIDTH-1:0]                   index_t;
-typedef logic [OFFSET_WIDTH-1]
-    
+typedef logic [OFFSET_WIDTH-1:0]                  offset_t;
+
+typedef logic [ASSOC_NUM-1:0]                     we_t;//每一路的写使能
+typedef logic [LINE_WORD_NUM-1:0][DATA_WIDTH-1:0] line_t;//每一路一个cache_line
+
+function index_t get_index( input logic [31:0] addr );
+    return addr[OFFSET_WIDTH + INDEX_WIDTH - 1 : OFFSET_WIDTH];
+endfunction
+
+function tag_t get_tag( input logic [31:0] addr );
+    return addr[31 : OFFSET_WIDTH + INDEX_WIDTH];
+endfunction
+
+function offset_t get_offset( input logic [31:0] addr );
+    return addr[OFFSET_WIDTH - 1 : 0];
+endfunction
+
+
+
+function logic [31:0] mux_byteenable(
+    input logic [31:0] rdata,
+    input logic [31:0] wdata,
+    input logic [3:0] sel 
+);
+    return { 
+        sel[3] ? wdata[31:24] : rdata[31:24],
+        sel[2] ? wdata[23:16] : rdata[23:16],
+        sel[1] ? wdata[15:8] : rdata[15:8],
+        sel[0] ? wdata[7:0] : rdata[7:0]
+    };
+endfunction
+
+typedef enum logic [3:0] { 
+    `ifdef Dcache
+
+        MISSDIRTY,
+        WRITEBACK,//之后加入fifo就不用这个装态了
+
+        REQ，
+        WAIT，
+    `endif
+
+        LOOKUP,
+        MISSCLEAN,
+        REFILL
+} state_t;
+
+`ifdef Dcache
+    typedef enum logic [2:0] { 
+        WB_IDLE,
+        WB_STORE
+    } wb_state_t;
+`endif 
+
+state_t state,state_next;
+`ifdef Dcache
+    wb_state_t wb_state,wb_state_next;
+`endif 
+
+
 endmodule
--- Src/NewCache/Rams.sv
+++ Src/NewCache/Rams.sv
@@ -1,154 +1,192 @@
 /*
  * @Author: your name
  * @Date: 2021-07-06 11:14:04
- * @LastEditTime: 2021-07-11 12:05:21
+ * @LastEditTime: 2021-07-12 12:19:51
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \NewCache\Rams.sv
  */
-module dual_port_ram #(
+module simple_port_lutram #(
 	// default data width if the fifo is of type logic
-	parameter int unsigned DATA_WIDTH = 32,
+	parameter int unsigned DATA_WIDTH  = 32,
 	// $bits(dtype) * SIZE = bits of the block RAM
-	parameter int unsigned SIZE       = 1024,
-	parameter type dtype              = logic [DATA_WIDTH-1:0],
-	parameter int unsigned LATENCY    = 1,
-	parameter int unsigned LATENCY_A  = LATENCY,
-	parameter int unsigned LATENCY_B  = LATENCY
+	parameter int unsigned SIZE        = 1024, //指有多少块
+	parameter type dtype               = logic [DATA_WIDTH-1:0],
+	parameter int unsigned LATENCY     = 0,
+	parameter int unsigned MEMORY_SIZE = $bits(dtype) * SIZE,
+	parameter int unsigned ADDR_WIDTH  = $clog2(SIZE)-1
 ) (
-	input  logic  clk,
-	input  logic  rst,
+	input  logic  clka,
+	input  logic  rsta,
 	input  logic  wea,
-	input  logic  web,
 	input  logic  ena,
-	input  logic  enb,
 	input  logic  [$clog2(SIZE)-1:0] addra,
-	input  logic  [$clog2(SIZE)-1:0] addrb,
 	input  dtype  dina,
-	input  dtype  dinb,
-	output dtype  douta,
-	output dtype  doutb
+	output dtype  douta
 );
 
-// xpm_memory_tdpram: True Dual Port RAM
-// Xilinx Parameterized Macro, Version 2016.2
-xpm_memory_tdpram #(
-	// Common module parameters
-	.MEMORY_SIZE($bits(dtype) * SIZE),
-	.MEMORY_PRIMITIVE("auto"),
-	.CLOCKING_MODE("common_clock"),
-	.USE_MEM_INIT(0),
-	.WAKEUP_TIME("disable_sleep"),
-	.MESSAGE_CONTROL(0),
+   xpm_memory_spram #(
+      .ADDR_WIDTH_A(ADDR_WIDTH),               // DECIMAL
+      .AUTO_SLEEP_TIME(0),                     // DECIMAL
+      .BYTE_WRITE_WIDTH_A($bits(dtype)),       // DECIMAL
+      .CASCADE_HEIGHT(0),            		   // DECIMAL
+      .ECC_MODE("no_ecc"),           		   // String
+      .MEMORY_INIT_FILE("none"),     		   // String
+      .MEMORY_INIT_PARAM("0"),       		   // String
+      .MEMORY_OPTIMIZATION("true"),  		   // String
+      .MEMORY_PRIMITIVE("auto"),     		   // String
+      .MEMORY_SIZE(MEMORY_SIZE),     		   // DECIMAL
+      .MESSAGE_CONTROL(0),           		   // DECIMAL
+      .READ_DATA_WIDTH_A($bits(dtype)),        // DECIMAL
+      .READ_LATENCY_A(LATENCY),            	   // DECIMAL
+      .READ_RESET_VALUE_A("0"),      		   // String
+      .RST_MODE_A("SYNC"),           		   // String
+      .SIM_ASSERT_CHK(0),            		   // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+      .USE_MEM_INIT(1),              		   // DECIMAL
+      .WAKEUP_TIME("disable_sleep"), 		   // String
+      .WRITE_DATA_WIDTH_A($bits(dtype)),       // DECIMAL
+      .WRITE_MODE_A("read_first")    		   // String
+   )
+   xpm_memory_spram_inst (
+      .dbiterra(),             // 1-bit output: Status signal to indicate double bit error occurrence
+                                       // on the data output of port A.
 
-	// Port A module parameters
-	.WRITE_DATA_WIDTH_A($bits(dtype)),
-	.READ_DATA_WIDTH_A($bits(dtype)),
-	.READ_RESET_VALUE_A("0"),
-	.READ_LATENCY_A(LATENCY_A),
-	.WRITE_MODE_A("write_first"),
+      .douta(douta),                   // READ_DATA_WIDTH_A-bit output: Data output for port A read operations.
+      .sbiterra(),             // 1-bit output: Status signal to indicate single bit error occurrence
+                                       // on the data output of port A.
 
-	// Port B module parameters
-	.WRITE_DATA_WIDTH_B($bits(dtype)),
-	.READ_DATA_WIDTH_B($bits(dtype)),
-	.READ_RESET_VALUE_B("0"),
-	.READ_LATENCY_B(LATENCY_B),
-	.WRITE_MODE_B("write_first")
-) xpm_mem (
-	// Common module ports
-	.sleep          ( 1'b0  ),
+      .addra(addra),                   // ADDR_WIDTH_A-bit input: Address for port A write and read operations.
+      .clka(clka),                     // 1-bit input: Clock signal for port A.
+      .dina(dina),                     // WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
+      .ena(ena),                       // 1-bit input: Memory enable signal for port A. Must be high on clock
+                                       // cycles when read or write operations are initiated. Pipelined
+                                       // internally.
 
-	// Port A module ports
-	.clka           ( clk   ),
-	.rsta           ( rst   ),
-	.ena            ( ena   ),
-	.regcea         ( 1'b0  ),
-	.wea            ( wea   ),
-	.addra          ( addra ),
-	.dina           ( dina  ),
-	.injectsbiterra ( 1'b0  ), // do not change
-	.injectdbiterra ( 1'b0  ), // do not change
-	.douta          ( douta ),
-	.sbiterra       (       ), // do not change
-	.dbiterra       (       ), // do not change
+      .injectdbiterra(1'b0), // 1-bit input: Controls double bit error injection on input data when
+                                       // ECC enabled (Error injection capability is not available in
+                                       // "decode_only" mode).
 
-	// Port B module ports
-	.clkb           ( clk   ),
-	.rstb           ( rst   ),
-	.enb            ( enb   ),
-	.regceb         ( 1'b0  ),
-	.web            ( web   ),
-	.addrb          ( addrb ),
-	.dinb           ( dinb  ),
-	.injectsbiterrb ( 1'b0  ), // do not change
-	.injectdbiterrb ( 1'b0  ), // do not change
-	.doutb          ( doutb ),
-	.sbiterrb       (       ), // do not change
-	.dbiterrb       (       )  // do not change
-);
+      .injectsbiterra(1'b0), // 1-bit input: Controls single bit error injection on input data when
+                                       // ECC enabled (Error injection capability is not available in
+                                       // "decode_only" mode).
+
+      .regcea(1'b0),                 // 1-bit input: Clock Enable for the last register stage on the output
+                                       // data path.
+
+      .rsta(rsta),                     // 1-bit input: Reset signal for the final port A output register stage.
+                                       // Synchronously resets output port douta to the value specified by
+                                       // parameter READ_RESET_VALUE_A.
+
+      .sleep(1'b0),                   // 1-bit input: sleep signal to enable the dynamic power saving feature.
+      .wea(wea)                        // WRITE_DATA_WIDTH_A/BYTE_WRITE_WIDTH_A-bit input: Write enable vector
+                                       // for port A input data port dina. 1 bit wide when word-wide writes are
+                                       // used. In byte-wide write configurations, each bit controls the
+                                       // writing one byte of dina to address addra. For example, to
+                                       // synchronously write only bits [15-8] of dina when WRITE_DATA_WIDTH_A
+                                       // is 32, wea would be 4'b0010.
+
+   );
 
 endmodule
 
-module dual_port_lutram #(
+module simple_port_ram #(
 	// default data width if the fifo is of type logic
 	parameter int unsigned DATA_WIDTH = 32,
+	parameter int unsigned LATENCY    = 0,
 	// $bits(dtype) * SIZE = bits of the block RAM
-	parameter int unsigned SIZE       = 1024,
-	parameter type dtype              = logic [DATA_WIDTH-1:0],
-	parameter int unsigned LATENCY    = 1,
-    parameter int unsigned LATENCY_A  = LATENCY,
-    parameter int unsigned LATENCY_B  = LATENCY
+	parameter int unsigned SIZE        = 1024, //指有多少块
+	parameter type dtype               = logic [DATA_WIDTH-1:0],
+	parameter int unsigned MEMORY_SIZE = $bits(dtype) * SIZE,
+	parameter int unsigned ADDR_WIDTH  = $clog2(SIZE)-1
 ) (
-	input  logic  clk,
-	input  logic  rst,
+	input  logic  clka,
+	input  logic  rsta,
 	input  logic  wea,
 	input  logic  ena,
 	input  logic  enb,
 	input  logic  [$clog2(SIZE)-1:0] addra,
 	input  logic  [$clog2(SIZE)-1:0] addrb,
 	input  dtype  dina,
-	output dtype  douta,
 	output dtype  doutb
 );
 
-// xpm_memory_dpdistram: Dual Port Distributed RAM
-// Xilinx Parameterized Macro, Version 2016.2
-xpm_memory_dpdistram #(
-	// Common module parameters
-	.MEMORY_SIZE($bits(dtype) * SIZE),
-	.CLOCKING_MODE("common_clock"),
-	.USE_MEM_INIT(0),
-	.MESSAGE_CONTROL(0),
+// xpm_memory_sdpram: Simple Dual Port RAM
+   // Xilinx Parameterized Macro, version 2019.2
 
-	// Port A module parameters
-	.WRITE_DATA_WIDTH_A($bits(dtype)),
-	.READ_DATA_WIDTH_A($bits(dtype)),
-	.READ_RESET_VALUE_A("0"),
-	.READ_LATENCY_A(LATENCY_A),
+   xpm_memory_sdpram #(
+      .ADDR_WIDTH_A(ADDR_WIDTH),               // DECIMAL
+      .ADDR_WIDTH_B(ADDR_WIDTH),               // DECIMAL
+      .AUTO_SLEEP_TIME(0),            // DECIMAL
+      .BYTE_WRITE_WIDTH_A($bits(dtype)),        // DECIMAL
+      .CASCADE_HEIGHT(0),             // DECIMAL
+      .CLOCKING_MODE("common_clock"), // String
+      .ECC_MODE("no_ecc"),            // String
+      .MEMORY_INIT_FILE("none"),      // String
+      .MEMORY_INIT_PARAM("0"),        // String
+      .MEMORY_OPTIMIZATION("true"),   // String
+      .MEMORY_PRIMITIVE("auto"),      // String
+      .MEMORY_SIZE(MEMORY_SIZE),             // DECIMAL
+      .MESSAGE_CONTROL(0),            // DECIMAL
+      .READ_DATA_WIDTH_B($bits(dtype)),         // DECIMAL
+      .READ_LATENCY_B(LATENCY),             // DECIMAL
+      .READ_RESET_VALUE_B("0"),       // String
+      .RST_MODE_A("SYNC"),            // String
+      .RST_MODE_B("SYNC"),            // String
+      .SIM_ASSERT_CHK(0),             // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+      .USE_EMBEDDED_CONSTRAINT(0),    // DECIMAL
+      .USE_MEM_INIT(1),               // DECIMAL
+      .WAKEUP_TIME("disable_sleep"),  // String
+      .WRITE_DATA_WIDTH_A($bits(dtype)),        // DECIMAL
+      .WRITE_MODE_B("write_first")      // String
+   )
+   xpm_memory_sdpram_inst (
+      .dbiterrb(),             // 1-bit output: Status signal to indicate double bit error occurrence
+                                       // on the data output of port B.
 
-	// Port B module parameters
-	.READ_DATA_WIDTH_B($bits(dtype)),
-	.READ_RESET_VALUE_B("0"),
-	.READ_LATENCY_B(LATENCY_B)
-) xpm_mem (
-	// Port A module ports
-	.clka           ( clk   ),
-	.rsta           ( rst   ),
-	.ena            ( ena   ),
-	.regcea         ( 1'b0  ),
-	.wea            ( wea   ),
-	.addra          ( addra ),
-	.dina           ( dina  ),
-	.douta          ( douta ),
+      .doutb(doutb),                   // READ_DATA_WIDTH_B-bit output: Data output for port B read operations.
+      .sbiterrb(),             // 1-bit output: Status signal to indicate single bit error occurrence
+                                       // on the data output of port B.
 
-	// Port B module ports
-	.clkb           ( clk   ),
-	.rstb           ( rst   ),
-	.enb            ( enb   ),
-	.regceb         ( 1'b0  ),
-	.addrb          ( addrb ),
-	.doutb          ( doutb )
-);
+      .addra(addra),                   // ADDR_WIDTH_A-bit input: Address for port A write operations.
+      .addrb(addrb),                   // ADDR_WIDTH_B-bit input: Address for port B read operations.
+      .clka(clka),                     // 1-bit input: Clock signal for port A. Also clocks port B when
+                                       // parameter CLOCKING_MODE is "common_clock".
+
+      .clkb(clkb),                     // 1-bit input: Clock signal for port B when parameter CLOCKING_MODE is
+                                       // "independent_clock". Unused when parameter CLOCKING_MODE is
+                                       // "common_clock".
+
+      .dina(dina),                     // WRITE_DATA_WIDTH_A-bit input: Data input for port A write operations.
+      .ena(ena),                       // 1-bit input: Memory enable signal for port A. Must be high on clock
+                                       // cycles when write operations are initiated. Pipelined internally.
+
+      .enb(enb),                       // 1-bit input: Memory enable signal for port B. Must be high on clock
+                                       // cycles when read operations are initiated. Pipelined internally.
+
+      .injectdbiterra(1'b0), // 1-bit input: Controls double bit error injection on input data when
+                                       // ECC enabled (Error injection capability is not available in
+                                       // "decode_only" mode).
+
+      .injectsbiterra(1'b0), // 1-bit input: Controls single bit error injection on input data when
+                                       // ECC enabled (Error injection capability is not available in
+                                       // "decode_only" mode).
+
+      .regceb(1'b0),                 // 1-bit input: Clock Enable for the last register stage on the output
+                                       // data path.
+
+      .rstb(rstb),                     // 1-bit input: Reset signal for the final port B output register stage.
+                                       // Synchronously resets output port doutb to the value specified by
+                                       // parameter READ_RESET_VALUE_B.
+
+      .sleep(1'b0),                   // 1-bit input: sleep signal to enable the dynamic power saving feature.
+      .wea(wea)                        // WRITE_DATA_WIDTH_A/BYTE_WRITE_WIDTH_A-bit input: Write enable vector
+                                       // for port A input data port dina. 1 bit wide when word-wide writes are
+                                       // used. In byte-wide write configurations, each bit controls the
+                                       // writing one byte of dina to address addra. For example, to
+                                       // synchronously write only bits [15-8] of dina when WRITE_DATA_WIDTH_A
+                                       // is 32, wea would be 4'b0010.
+
+   );
 
 endmodule
 
