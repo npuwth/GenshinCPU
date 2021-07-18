f17a44742661ad03226e3ef20b310c30c30f64f0
npuwth <1985871043@qq.com>
2021-07-18 15:18:16 +0800

    试一下uncache[fbit]

--- Src/refactor/CommonDefines.svh
+++ Src/refactor/CommonDefines.svh
@@ -1,8 +1,8 @@
 /*
  * @Author: Johnson Yang
  * @Date: 2021-03-24 14:40:35
- * @LastEditTime: 2021-07-18 02:15:06
- * @LastEditors: Johnson Yang
+ * @LastEditTime: 2021-07-18 15:17:49
+ * @LastEditors: npuwth
  * @Copyright 2021 GenshinCPU
  * @Version:1.0
  * @IO PORT:
@@ -14,7 +14,7 @@
 // 取消功能,需要将`define注释掉
 // `define FPU_DETECT_EN       1  // 定义则打开浮点检测功能
 // `define DEBUG               1        
-// `define All_Uncache         1  // 定义则全走uncache     
+`define All_Uncache         1  // 定义则全走uncache     
 
 `define ALUOpLen            4:0
 `define InstrLen            31:0
