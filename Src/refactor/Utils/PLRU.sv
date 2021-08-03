c5fd4dba875f8676f3b03f475b5768c2a5b5a936
jjx <jshmjjx@126.com>
2021-08-03 15:50:24 +0800

    Update PLRU.sv

--- Src/refactor/Utils/PLRU.sv
+++ Src/refactor/Utils/PLRU.sv
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-06-29 23:14:40
- * @LastEditTime: 2021-07-15 10:20:31
- * @LastEditors: npuwth
+ * @LastEditTime: 2021-08-03 10:58:27
+ * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \Src\Code\NewCache\plru.sv
  */
@@ -20,7 +20,7 @@ module PLRU #(
     output [$clog2(ASSOC_NUM)-1:0] lru  //表示 这次如果替换 替换哪一路
 );
 
-logic [ASSOC_NUM-2:0] state, state_d;
+logic [ASSOC_NUM-2:0] state, state_next;
 
 // Assign output
 generate
@@ -35,36 +35,36 @@ endgenerate
 generate
 if(ASSOC_NUM == 2) begin
     always_comb begin
-        state_d = state;
+        state_next = state;
 
         if(update && |access) begin
             if(access[0]) begin
-                state_d[0] = 1;//如果这次命中的是第0路 那么下次不命中的时候替换的就是1路
+                state_next[0] = 1;//如果这次命中的是第0路 那么下次不命中的时候替换的就是1路
             end else begin
-                state_d[0] = 0;
+                state_next[0] = 0;
             end
         end
     end
 end else  begin
     always_comb begin
-        state_d = state;    //好习惯啊
+        state_next = state;    //好习惯啊
 
         casez(access)
             4'b1???: begin
-                state_d[2] = 1'b0;
-                state_d[0] = 1'b0;
+                state_next[2] = 1'b0;
+                state_next[0] = 1'b0;
             end
             4'b01??: begin
-                state_d[2] = 1'b0;
-                state_d[0] = 1'b1;
+                state_next[2] = 1'b0;
+                state_next[0] = 1'b1;
             end
             4'b001?: begin
-                state_d[2] = 1'b1;
-                state_d[1] = 1'b0;
+                state_next[2] = 1'b1;
+                state_next[1] = 1'b0;
             end
             4'b0001: begin
-                state_d[2] = 1'b1;
-                state_d[1] = 1'b1;
+                state_next[2] = 1'b1;
+                state_next[1] = 1'b1;
             end
         endcase
     end
@@ -75,7 +75,7 @@ always_ff @(posedge clk) begin
     if(resetn == `RstEnable) begin
         state <= '0;
     end else if(update) begin
-        state <= state_d;
+        state <= state_next;
     end
 end
 
