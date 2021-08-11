70afc228cfad46d317528b65bd6ce8b6574d4d29
jjx <jshmjjx@126.com>
2021-08-11 15:13:43 +0800

    Update Decode.sv

--- Src/refactor/ID/Decode.sv
+++ Src/refactor/ID/Decode.sv
@@ -1,7 +1,7 @@
 /*
  * @Author: Juan Jiang
  * @Date: 2021-04-02 09:40:19
- * @LastEditTime: 2021-08-10 22:33:14
+ * @LastEditTime: 2021-08-11 14:13:34
  * @LastEditors: Please set LastEditors
  * @Copyright 2021 GenshinCPU
  * @Version:1.0
@@ -360,7 +360,7 @@ module Decode(
 			        	3'b010:  instrType = OP_SWL;
 			        	3'b011:  instrType = OP_SW;
 			        	3'b110:  instrType = OP_SWR;
-			        	3'b111:  instrType = OP_CACHE;
+			        	3'b111:  instrType = OP_NOP;
 			        	default: instrType = OP_INVALID;
 			        endcase
 		        end
