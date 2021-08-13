ede0120dbbef11da5e3cacb91272758e849fd19c
jjx <jshmjjx@126.com>
2021-08-13 15:49:59 +0800

    增加了victim_cache

--- Src/refactor/Utils/Victim_Cache.sv
+++ Src/refactor/Utils/Victim_Cache.sv
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-08-08 11:23:21
- * @LastEditTime: 2021-08-10 12:04:37
+ * @LastEditTime: 2021-08-13 14:00:05
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \gitlab\Src\refactor\Utils\Victim_Cache.sv
@@ -15,25 +15,26 @@ module Victim_Cache #(
 ) (
     input logic clk,
     input logic resetn,
-    input logic[INDEX_WIDTH-1:0] index,
-    input logic we,
-    input logic[INDEX_WIDTH-1:0] index_wdata,
-    input logic[TAG_WIDTH+1-1:0] tagv_wdata,
-    input logic [ASSOC_NUM-1:0][LINE_WORD_NUM-1:0] data_wdata,
-    output logic [ASSOC_NUM-1:0][LINE_WORD_NUM-1:0] data_rdata,
-    output logic[TAG_WIDTH+1-1:0] tagv_rdata,
-    output logic [INDEX_WIDTH-1:0] index_rdata
+    input logic[INDEX_WIDTH-1:0] index,         //用于索引
+    input logic data_read_en,
+    input logic we,                             //用于替换时的写使能
+    // input logic[INDEX_WIDTH-1:0] index_wdata,   //victim的index
+    input logic[TAG_WIDTH+INDEX_WIDTH+1-1:0] tagvindex_wdata,    //victim的tagv
+    input logic [LINE_WORD_NUM-1:0][31:0] data_wdata,//victim的data
+    output logic [LINE_WORD_NUM-1:0][31:0] data_rdata,//读出的data
+    output logic[TAG_WIDTH+INDEX_WIDTH+1-1:0] tagvindex_rdata //读出的tagv_rdata
 );
     
 typedef struct packed {
     logic valid;
     logic [TAG_WIDTH-1:0] tag;  
-} tagv_t; //每一路 一个tag_t变量
+    logic [INDEX_WIDTH-1:0] index;
+} tagvindex_t; //每一路 一个tag_t变量
 
 //
 simple_port_lutram  #(
     .SIZE(SIZE),
-    .dtype(tagv_t)
+    .dtype(tagvindex_t)
 ) mem_tag(
     .clka(clk),
     .rsta(~resetn),
@@ -42,26 +43,28 @@ simple_port_lutram  #(
     .ena(1'b1),
     .wea(we),
     .addra(index[$clog2(SIZE)-1:0]),
-    .dina(tagv_wdata),
-    .douta(tagv_rdata)
+    .dina(tagvindex_wdata),
+    .douta(tagvindex_rdata)
 );
-
-    simple_port_ram #(
-        .SIZE(SET_NUM)
+for (genvar  i=0; i<LINE_WORD_NUM; ++i) begin
+        simple_port_ram #(
+        .SIZE(SIZE)
     )mem_data(
         .clk(clk),
         .rst(~resetn),
 
         //写端�?
         .ena(1'b1),
-        .wea(data_we[i][j]),
-        .addra(write_addr),
+        .wea(we),
+        .addra(index[$clog2(SIZE)-1:0]),
         .dina(data_wdata[j]),
 
         //读端�?
         .enb(data_read_en),
-        .addrb(read_addr),
-        .doutb(data_rdata[i][j])
+        .addrb(index[$clog2(SIZE)-1:0]),
+        .doutb(data_rdata[i])
     );
+end
+
 
 endmodule
